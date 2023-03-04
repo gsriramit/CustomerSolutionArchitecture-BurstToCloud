802715beafa46748920fef60c94053b4319fd8ca
Divakar Kumar <divakar.divakar.k@gmail.com>
2023-03-04 23:43:31 +0530

    dynamic connectionstring

--- SampleApplication/BurstToCloudApplication/DotNetCoreSqlDb/Data/MyDatabaseContext.cs
+++ SampleApplication/BurstToCloudApplication/DotNetCoreSqlDb/Data/MyDatabaseContext.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DotNetCoreSqlDb.Models
 {
     public class MyDatabaseContext : DbContext
     {
-        public MyDatabaseContext (DbContextOptions<MyDatabaseContext> options)
+        private readonly HttpContext _httpContext;
+        private readonly IConfiguration _configuration;
+        public MyDatabaseContext(DbContextOptions<MyDatabaseContext> options, IHttpContextAccessor httpContextAccessor, IConfiguration configuration)
             : base(options)
         {
+            _httpContext = httpContextAccessor.HttpContext;
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (_httpContext != null)
+            {
+                if (_httpContext.Request.Method == "GET")
+                {
+                    optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ReadOnlyDbConnection"));
+                }
+                else
+                {
+                    optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ReadWriteDbConnection"));
+                }
+            }
         }
 
         public DbSet<DotNetCoreSqlDb.Models.Todo> Todo { get; set; }
---
+++ SampleApplication/BurstToCloudApplication/DotNetCoreSqlDb/Migrations/20230304171936_InitAzReadWriteSql.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotNetCoreSqlDb.Migrations
+{
+    public partial class InitAzReadWriteSql : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
